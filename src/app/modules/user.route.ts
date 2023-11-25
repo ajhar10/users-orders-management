7623479b1b11182865a10a65d39246185e4e606b
Your Name <you@example.com>
2023-11-25 22:24:59 +0600

    route add

--- src/app/modules/user.route.ts
+++ src/app/modules/user.route.ts
@@ -11,6 +11,18 @@ router.get("/:userId", UserControllers.getSingleUser);
 
 router.put("/:userId", UserControllers.updateSingleUser);
 
-// router.delete('/:studentId', UserControllers.delSingleStudent)
+router.put("/:userId", UserControllers.updateUserData);
+//Delete user
+router.delete("/:userId", UserControllers.deleteUserData);
+//Insert order user collection
+router.put("/:userId/orders", UserControllers.insertOrderCollection);
+//Get user order Data
+router.get("/:userId/orders", UserControllers.getUserOrderData);
+
+//User order price calculate
+router.get(
+  "/:userId/orders/total-price",
+  UserControllers.CalculateAllUserOrder
+);
 
 export const UserRoutes = router;
--- src/app/modules/user.service.ts
+++ src/app/modules/user.service.ts
@@ -33,9 +33,32 @@ const getSingleUserFromDB = async (id: string) => {
     throw new Error("User not found");
   }
 };
+const updateSingleUserFromDB = async (id: string) => {
+  if (await User.isUserExists(id)) {
+    const result = await User.aggregate([
+      { $match: { userId: Number(id) } },
+      {
+        $project: {
+          userId: 1,
+          username: 1,
+          fullName: 1,
+          age: 1,
+          email: 1,
+          isActive: 1,
+          address: 1,
+          hobbies: 1,
+        },
+      },
+    ]);
+    return result;
+  } else {
+    throw new Error("User not found");
+  }
+};
 
 export const UserServices = {
   createUserIntoDB,
   getAllUsersFromDB,
   getSingleUserFromDB,
+  updateSingleUserFromDB,
 };
